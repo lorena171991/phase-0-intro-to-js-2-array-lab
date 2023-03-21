8cf99f01142f110f536326ee4527a8955ea23cf7
Lorena Aragon <lorena171991@yahoo.com>
2023-03-20 23:21:12 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(name) {
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(name) {
+    cats.pop(name)
+}
+
+function destructivelyRemoveFirstCat(name) {
+    cats.shift(name)
+}
+
+function appendCat(name) {
+    const copyCats = [...cats];
+    copyCats.push(name)
+    return(copyCats)
+}
+
+function prependCat(name) {
+    const copyCats = [...cats];
+    copyCats.unshift(name)
+    return(copyCats)
+}
+
+
+function removeLastCat(name) {
+    const copyCats = [...cats];
+    copyCats.pop(name)
+    return(copyCats) 
+}
+
+function removeFirstCat(name) {
+    const copyCats = [...cats];
+    copyCats.shift(name)
+    return(copyCats)
+}
